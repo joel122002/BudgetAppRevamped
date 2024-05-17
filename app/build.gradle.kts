9fe07506157e899074ceb7f8b31f2a21d652d6dc
Joel <joel02varghese@gmail.com>
2024-05-17 23:33:18 +0530

    Add firestore dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,4 +78,7 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics")
+    // Declare the dependency for the Cloud Firestore library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-firestore")
 }

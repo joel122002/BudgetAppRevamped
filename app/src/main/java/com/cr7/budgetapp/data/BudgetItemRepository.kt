7b3712c18aff991d39e6167f44658d1efe1ceb79
Joel <joel02varghese@gmail.com>
2024-07-20 13:23:24 +0530

    Update repository

--- app/src/main/java/com/cr7/budgetapp/data/BudgetItemRepository.kt
+++ app/src/main/java/com/cr7/budgetapp/data/BudgetItemRepository.kt
@@ -3,20 +3,63 @@ package com.cr7.budgetapp.data
 import androidx.lifecycle.LiveData
 import com.cr7.budgetapp.data.local.BudgetItem
 import com.cr7.budgetapp.data.local.BudgetItemDao
+import com.cr7.budgetapp.data.remote.FirebaseService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import java.util.Date
 
-class BudgetItemRepository(private val budgetItemDao: BudgetItemDao) {
-    suspend fun getData(start: Date, end: Date): LiveData<BudgetItem> {
+class BudgetItemRepository(
+    private val budgetItemDao: BudgetItemDao
+) {
+    private val firebaseService = FirebaseService()
+    fun getData(start: Date, end: Date): Flow<List<BudgetItem>> {
         return budgetItemDao.getBetweenDate(start, end)
     }
 
-    suspend fun refreshData() {
-        var remoteData:LiveData<BudgetItem>
-//        remoteData.forEach { budgetItemDao.insert(it) }
+    /**
+     * Function to insert new records and to update records that already exist with the record that has the newest updated_at
+     */
+    suspend fun refreshData(start: Date, end: Date) {
+        // Server Items. Has all items that would exist in existingItems
+        val budgetItems = firebaseService.getBudgetItemsBetweenDate(start, end)
+        val ids = budgetItems.map { it.uid }
+        // Database Items. Subset of budgetItems
+        val existingItems = budgetItemDao.getItemsBUIDs(ids)
+        val existingItemsMap = existingItems.associateBy { it.uid }
+
+        // Creates a list of elements that either
+        // 1. Do not exist in the database
+        // 2. If exists chooses the item with the larger updatedAt
+        val insertOrUpdateItems = budgetItems.map {
+            if (existingItemsMap[it.uid]?.uid != it.uid)
+                it
+            else
+                    (if (existingItemsMap[it.uid]?.updatedAt!! > it.updatedAt)
+                        existingItemsMap[it.uid]!!
+                    else
+                        it)
+        }
+
+        withContext(Dispatchers.IO) {
+            budgetItemDao.insertMany(insertOrUpdateItems)
+            budgetItemDao.deleteRecordsNotInFirebase(ids)
+            val unsyncedItems = budgetItemDao.getUnsyncedItems()
+            unsyncedItems.forEach { budgetItem ->
+                budgetItem.sync = true
+                firebaseService.insertBudgetItem(budgetItem)
+                budgetItemDao.insert(budgetItem)
+            }
+        }
     }
 
     suspend fun insert(budgetItem: BudgetItem) {
-        budgetItemDao.insert(budgetItem)
+        withContext(Dispatchers.IO) {
+            budgetItemDao.insert(budgetItem)
+            budgetItem.sync = true
+            firebaseService.insertBudgetItem(budgetItem)
+            budgetItemDao.insert(budgetItem)
+        }
     }
 
     suspend fun delete(budgetItem: BudgetItem) {

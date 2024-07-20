669183094da2f31dd7fe7069aaeaeece522d2465
Joel <joel02varghese@gmail.com>
2024-07-20 13:10:51 +0530

    Add fucntions to get items based on uid, get unsynced items, insert many and delete items

--- app/src/main/java/com/cr7/budgetapp/data/local/BudgetItemDao.kt
+++ app/src/main/java/com/cr7/budgetapp/data/local/BudgetItemDao.kt
@@ -6,17 +6,31 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Update
 import com.cr7.budgetapp.data.local.BudgetItem
+import kotlinx.coroutines.flow.Flow
 import java.util.Date
 
 @Dao
 interface BudgetItemDao {
-    @Query("SELECT * FROM budget WHERE created_at BETWEEN :start AND :end")
-    fun getBetweenDate(start: Date, end: Date): LiveData<BudgetItem>
+    @Query("SELECT * FROM budget WHERE date BETWEEN :start AND :end")
+    fun getBetweenDate(start: Date, end: Date): Flow<List<BudgetItem>>
+
+    @Query("SELECT * FROM budget WHERE uid IN (:uids)")
+    suspend fun getItemsBUIDs(uids: List<String>): List<BudgetItem>
+
+    @Query("SELECT * FROM budget WHERE sync=0")
+    fun getUnsyncedItems(): List<BudgetItem>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(budgetItem: BudgetItem)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertMany(budgetItems: List<BudgetItem>)
+
+    @Query("DELETE FROM budget WHERE uid NOT IN (:uids) AND sync=1")
+    suspend fun deleteRecordsNotInFirebase(uids: List<String>)
 
     @Update
     fun update(budgetItem: BudgetItem)

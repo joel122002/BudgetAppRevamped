aa400214aa7dcb39a6a55d53c3ef587c1b482f4f
Joel <joel02varghese@gmail.com>
2024-07-20 11:28:49 +0530

    Update ButgetItem with new fields. Add empty constructor(for mapping firebase objects) and constructor without uid

--- app/src/main/java/com/cr7/budgetapp/data/local/BudgetItem.kt
+++ app/src/main/java/com/cr7/budgetapp/data/local/BudgetItem.kt
@@ -10,9 +10,14 @@ import java.util.UUID
 data class BudgetItem (
     @PrimaryKey val uid: String = UUID.randomUUID().toString(),
     @ColumnInfo(name = "created_at") val createdAt: Date,
-    @ColumnInfo(name = "updated_at") val updatedAt: Date?,
-    @ColumnInfo(name = "name") val name: String,
-    @ColumnInfo(name = "price") val price: Float,
+    @ColumnInfo(name = "updated_at") var updatedAt: Date,
+    @ColumnInfo(name = "date") val date: Date,
+    @ColumnInfo(name = "name") var name: String,
+    @ColumnInfo(name = "price") var price: Float,
+    @ColumnInfo(name = "user_id") val userId: String,
     // Determines if has been updated with server
-    @ColumnInfo(name = "sync") val sync: Boolean = false
-)
+    @ColumnInfo(name = "sync") var sync: Boolean = false
+) {
+    constructor(): this("",Date(), Date(), Date(), "", 0.0f, "",false)
+    constructor(createdAt: Date, updatedAt: Date, date: Date, name: String, price: Float, sync: Boolean): this(UUID.randomUUID().toString(),createdAt, updatedAt, date, name, price, "joelv1202",sync)
+}

10a2b4a605c3527b638237486a6abebcfcd12af2
Joel <joel02varghese@gmail.com>
2024-07-28 20:50:44 +0530

    Change userId to userDoc where userDoc is a Document reference and not in the database

--- app/src/main/java/com/cr7/budgetapp/data/local/BudgetItem.kt
+++ app/src/main/java/com/cr7/budgetapp/data/local/BudgetItem.kt
@@ -2,22 +2,24 @@ package com.cr7.budgetapp.data.local
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
+import com.google.firebase.firestore.DocumentReference
 import java.util.Date
 import java.util.UUID
 
 @Entity(tableName = "budget")
 data class BudgetItem (
-    @PrimaryKey val uid: String = UUID.randomUUID().toString(),
-    @ColumnInfo(name = "created_at") val createdAt: Date,
+    @PrimaryKey var uid: String = UUID.randomUUID().toString(),
+    @ColumnInfo(name = "created_at") var createdAt: Date,
     @ColumnInfo(name = "updated_at") var updatedAt: Date,
-    @ColumnInfo(name = "date") val date: Date,
+    @ColumnInfo(name = "date") var date: Date,
     @ColumnInfo(name = "name") var name: String,
     @ColumnInfo(name = "price") var price: Float,
-    @ColumnInfo(name = "user_id") val userId: String,
+    @Ignore var userDoc: DocumentReference?,
     // Determines if has been updated with server
     @ColumnInfo(name = "sync") var sync: Boolean = false
 ) {
-    constructor(): this("",Date(), Date(), Date(), "", 0.0f, "",false)
-    constructor(createdAt: Date, updatedAt: Date, date: Date, name: String, price: Float, sync: Boolean): this(UUID.randomUUID().toString(),createdAt, updatedAt, date, name, price, "joelv1202",sync)
+    constructor(): this("",Date(), Date(), Date(), "", 0.0f, null,false)
+    constructor(createdAt: Date, updatedAt: Date, date: Date, name: String, price: Float, userDoc: DocumentReference?, sync: Boolean): this(UUID.randomUUID().toString(),createdAt, updatedAt, date, name, price, userDoc,sync)
 }

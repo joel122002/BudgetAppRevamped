d3e34e69edf1e08dabf252fe7baa9f1564416ffd
Joel <joel02varghese@gmail.com>
2024-08-05 21:21:11 +0530

    Implement navigation

--- app/src/main/java/com/cr7/budgetapp/ui/screens/main/MainActivity.kt
+++ app/src/main/java/com/cr7/budgetapp/ui/screens/main/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.cr7.budgetapp.ui.screens.DetailsScreen
+import com.cr7.budgetapp.ui.screens.calculate.CalculateScreen
+import com.cr7.budgetapp.ui.screens.helpers.LocalNavController
+import com.cr7.budgetapp.ui.screens.helpers.Routes
 import com.cr7.budgetapp.ui.screens.signin.SignInScreen
 import com.cr7.budgetapp.ui.viewmodel.AuthViewModel
 
@@ -22,7 +29,6 @@ class MainActivity : ComponentActivity() {
         val authViewModel = AuthViewModel()
 
         setContent {
-
             val userState by authViewModel.authState.collectAsState()
             val userDoc by authViewModel.userDoc.collectAsState()
             LaunchedEffect(userState) {
@@ -34,7 +40,15 @@ class MainActivity : ComponentActivity() {
                 if (userDoc != null && !userDoc!!.exists()) {
                     DetailsScreen(authViewModel = authViewModel)
                 } else {
-                    MainComposable(application = application, lifecycleScope = lifecycleScope, /*budgetItemViewModel = budgetItemViewModel*/)
+                    val navController = rememberNavController()
+                    CompositionLocalProvider(LocalNavController provides navController) {
+                        NavHost(navController = navController, startDestination = Routes.budget.route) {
+                            composable(Routes.budget.route) { MainComposable(application = application, lifecycleScope = lifecycleScope) }
+                            composable(Routes.calculate.route) { CalculateScreen() }
+                            // Add more destinations similarly.
+                        }
+                    }
+
                 }
             } else {
                 SignInScreen(activityContext = this, lifecycleScope = lifecycleScope, authViewModel = authViewModel)

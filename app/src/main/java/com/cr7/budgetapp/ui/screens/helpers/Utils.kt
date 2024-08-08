a5853f8caa8851c8e94852b1052d395a59ce32fa
Joel <joel02varghese@gmail.com>
2024-08-08 14:11:05 +0530

    Add helper functions for laundry screen

--- app/src/main/java/com/cr7/budgetapp/ui/screens/helpers/Utils.kt
+++ app/src/main/java/com/cr7/budgetapp/ui/screens/helpers/Utils.kt
@@ -4,6 +4,7 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.ZoneId
+import java.util.Calendar
 import java.util.Date
 
 fun getTomorrowAtMidnight(): Date {
@@ -43,4 +44,28 @@ fun getMidnight(date: Date): Date {
     val midnight = LocalTime.MIDNIGHT
     val tomorrowAtMidnight = LocalDateTime.of(localDate, midnight)
     return Date.from(tomorrowAtMidnight.atZone(ZoneId.of("UTC")).toInstant())
+}
+
+fun getFirstDayOfMonth(month: Int, year: Int): Date {
+    // Create an instance of Calendar
+    val calendar = Calendar.getInstance()
+
+    // Set the calendar to the first day of the specified month and year
+    calendar.set(Calendar.YEAR, year)
+    calendar.set(Calendar.MONTH, month)
+    calendar.set(Calendar.DAY_OF_MONTH, 1)
+
+    // Return the Date object representing the first day of the month
+    return calendar.time
+}
+
+fun getCurrentMonth(): Int {
+    val calendar = Calendar.getInstance()
+    // Note: Calendar.MONTH is zero-based, so January is 0
+    return calendar.get(Calendar.MONTH)
+}
+
+fun getCurrentYear(): Int {
+    val calendar = Calendar.getInstance()
+    return calendar.get(Calendar.YEAR)
 }

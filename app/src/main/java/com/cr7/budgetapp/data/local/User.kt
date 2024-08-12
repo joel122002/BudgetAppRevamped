f64d18af20c37af6c1b670159f1689cd6f77793a
Joel <joel02varghese@gmail.com>
2024-08-12 21:36:55 +0530

    Make user a database class by adding path to resolve username from path

--- app/src/main/java/com/cr7/budgetapp/data/local/User.kt
+++ app/src/main/java/com/cr7/budgetapp/data/local/User.kt
@@ -1,5 +1,14 @@
 package com.cr7.budgetapp.data.local
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.firebase.firestore.Exclude
+
+@Entity(tableName = "user")
 data class User(
-    val username: String
-)
+    var username: String,
+    @Exclude @PrimaryKey var path: String
+) {
+    constructor(): this("", "")
+    constructor(path: String): this("", path)
+}

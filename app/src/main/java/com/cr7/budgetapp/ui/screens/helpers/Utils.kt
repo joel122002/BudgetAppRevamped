4c2e783b345e163a8acfa68197c574d6c1f365a9
Joel <joel02varghese@gmail.com>
2024-08-13 13:11:56 +0530

    Create utility functions to create CSV

--- app/src/main/java/com/cr7/budgetapp/ui/screens/helpers/Utils.kt
+++ app/src/main/java/com/cr7/budgetapp/ui/screens/helpers/Utils.kt
@@ -1,5 +1,23 @@
 package com.cr7.budgetapp.ui.screens.helpers
 
+import android.app.Application
+import android.app.DownloadManager
+import android.content.ContentValues
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.annotation.RequiresApi
+import androidx.core.net.toUri
+import com.cr7.budgetapp.data.local.BudgetItem
+import com.cr7.budgetapp.ui.viewmodel.AuthViewModel
+import com.cr7.budgetapp.ui.viewmodel.UserViewModel
+import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
@@ -68,4 +86,119 @@ fun getCurrentMonth(): Int {
 fun getCurrentYear(): Int {
     val calendar = Calendar.getInstance()
     return calendar.get(Calendar.YEAR)
+}
+
+@RequiresApi(Build.VERSION_CODES.Q)
+fun saveToDownloads(fileUri: Uri, application: Application, fileName: String) {
+    val contentValues = ContentValues()
+    // Enter the name of the file here. Note the extension isn't necessary
+    contentValues.put(MediaStore.Downloads.DISPLAY_NAME, fileName);
+    // Here we define the file type. Do check the MIME_TYPE for your file. For jpegs it is "image/jpeg"
+    contentValues.put(MediaStore.Downloads.MIME_TYPE, "text/csv");
+    contentValues.put(MediaStore.Downloads.IS_PENDING, true);
+    val downloadsUri = MediaStore.Downloads.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+    val destinationFileUri =
+        application.contentResolver.insert(downloadsUri, contentValues) ?: return
+    val inputStream = application.contentResolver.openInputStream(fileUri) ?: return
+    val outputStream = application.contentResolver.openOutputStream(destinationFileUri) ?: return
+    val buf = ByteArray(1024)
+    var len: Int
+    while (inputStream.read(buf).also { len = it } > 0) {
+        outputStream.write(buf, 0, len)
+    }
+    inputStream.close()
+    outputStream.close()
+    contentValues.put(MediaStore.Images.Media.IS_PENDING, false)
+    application.contentResolver.update(destinationFileUri, contentValues, null, null)
+}
+
+fun legacySaveToDownloads(uri: Uri, application: Application, fileName: String) {
+    val request = DownloadManager.Request(uri)
+    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
+    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED) // to notify when download is complete f you want to be available from media players
+    val manager =
+        application.baseContext.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager?
+    if (manager == null)
+        return
+    manager.enqueue(request)
+}
+
+fun generateFilenamePrefix(start: Date, end: Date): String {
+    val startDateString = SimpleDateFormat("dd MMMM yyyy").format(start)
+    val endDateString = SimpleDateFormat("dd MMMM yyyy").format(end)
+    return "$startDateString - $endDateString"
+}
+
+suspend fun createCurrentUserCSV(
+    start: Date,
+    end: Date,
+    userMap: Map<String, String>,
+    budgetItems: List<BudgetItem>,
+    userViewModel: UserViewModel,
+    authViewModel: AuthViewModel,
+    application: Application,
+) {
+    withContext(Dispatchers.IO) {
+        val rows = mutableListOf(listOf("Date", "Item", "Price", "User"))
+        userViewModel.resolveUsers(budgetItems)
+        budgetItems.forEach { budgetItem ->
+            if (budgetItem.userDoc != null && budgetItem.userDoc!!.path == authViewModel.getUserDocumentRef().path) {
+                rows.add(
+                    listOf(
+                        SimpleDateFormat("dd/MM/yyyy").format(budgetItem.date),
+                        budgetItem.name,
+                        budgetItem.price.toString(),
+                        (userMap[budgetItem.userDoc!!.path] ?: "")
+                    )
+                )
+            }
+        }
+        val path = application.baseContext.getFilesDir()
+        csvWriter().writeAll(rows = rows, "$path/test.csv")
+        val uri = File("$path/test.csv").toUri()
+        val filename = "${
+            generateFilenamePrefix(
+                start,
+                end
+            )
+        } ${userMap[authViewModel.getUserDocumentRef().path]}.csv"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            saveToDownloads(uri, application, filename)
+        } else {
+            legacySaveToDownloads(uri, application, filename)
+        }
+    }
+}
+
+suspend fun createAllUserCSV(
+    start: Date,
+    end: Date,
+    userMap: Map<String, String>,
+    budgetItems: List<BudgetItem>,
+    userViewModel: UserViewModel,
+    application: Application,
+) {
+    withContext(Dispatchers.IO) {
+        val rows = mutableListOf(listOf("Date", "Item", "Price", "User"))
+        userViewModel.resolveUsers(budgetItems)
+        budgetItems.forEach { budgetItem ->
+            rows.add(
+                listOf(
+                    SimpleDateFormat("dd/MM/yyyy").format(budgetItem.date),
+                    budgetItem.name,
+                    budgetItem.price.toString(),
+                    (userMap[budgetItem.userDoc!!.path] ?: "")
+                )
+            )
+        }
+        val path = application.baseContext.getFilesDir()
+        csvWriter().writeAll(rows = rows, "$path/test.csv")
+        val uri = File("$path/test.csv").toUri()
+        val filename = "${generateFilenamePrefix(start, end)} All Users.csv"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            saveToDownloads(uri, application, filename)
+        } else {
+            legacySaveToDownloads(uri, application, filename)
+        }
+    }
 }
